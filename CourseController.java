3ae9788ba6439fbaaf897993f32b6050745b8bb3
Leeshan-L <leeshansasanjana00@gmail.com>
2025-05-10 01:10:40 +0530

    Final Submission

---
+++ CourseController.java
@@ -0,0 +1,39 @@
+package com.university.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import com.university.dao.CourseDAO;
+import com.university.dao.RegistrationDAO;
+import com.university.model.Course;
+import com.university.model.Student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class CourseController {
+
+    @Autowired
+    private CourseDAO courseDAO;
+
+    @Autowired
+    private RegistrationDAO registrationDAO;
+
+
+    @GetMapping("/courses")
+    public String listCourses(Model model) {
+        List<Course> courses = courseDAO.getAllCourses();
+        model.addAttribute("courses", courses);
+        return "courses";
+    }
+
+    @PostMapping("/register/{courseId}")
+    public String register(@PathVariable int courseId, HttpSession session) {
+        Student student = (Student) session.getAttribute("student");
+        registrationDAO.registerCourse(student.getStudentId(), courseId);
+        return "success";
+}
